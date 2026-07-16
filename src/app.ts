80dab02138659b294c31df79cbdbf67e41a31ec3
aashraful349 <aashraful349@gmail.com>
2026-07-16 23:57:21 +0600

    added create user

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import express,{ Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import httpStatus from "http-status";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
+import { userRoutes } from "./modules/users/user.route";
+
+const app:Application=express();
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+
+
+app.get("/",async(req:Request,res:Response)=>{
+    res.send("Hello World");
+})
+
+app.use("/api/users",userRoutes)
+
+export default app;
---
+++ src/modules/users/user.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs";
+import config from "../../config";
+import httpStatus from "http-status";
+import { userService } from "./user.service";
+
+
+const registerUser=async(req:Request,res:Response)=>{
+   try {
+     const payload=req.body;
+    // console.log({name,email,password,profilePhoto});
+
+   const user=await userService.registerUserIntoDB(payload);
+
+    res.status(httpStatus.CREATED).json(
+        {
+            success:true,
+            statusCode:httpStatus.CREATED,
+            message:"User registered successfully",
+            data:{
+                user
+            }
+        }
+    )
+   } catch (error) {
+    // console.log(error);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(
+        {
+            success:false,
+            statusCode:httpStatus.INTERNAL_SERVER_ERROR,
+            message:"Internal server error",
+            error:(error as Error).message
+        }
+    )
+   }
+}
+
+
+export const userController={
+    registerUser
+}
