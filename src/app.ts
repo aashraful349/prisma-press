fd430f35b5ff258a5b32a6ebfdaae5343544dde6
aashraful349 <aashraful349@gmail.com>
2026-07-27 10:19:54 +0600

    implemented global error handeler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cookieParser from "cookie-parser";
-import express,{ Application, Request, Response } from "express";
+import express,{ Application, ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import httpStatus from "http-status";
@@ -9,6 +9,8 @@ import { userRoutes } from "./modules/users/user.route";
 import { authRoutes } from "./modules/auth/auth.routes";
 import { postRoutes } from "./modules/post/post.route";
 import { commentRoutes } from "./modules/comment/comment.route";
+import { notFound } from "./middlewares/notFound";
+import { globalErrorHandler } from "./middlewares/globalErrorHandeler";
 
 const app:Application=express();
 
@@ -35,5 +37,20 @@ app.use("/api/auth",authRoutes)
 
 app.use("/api/posts",postRoutes);
 app.use("/api/comments",commentRoutes);
+
+app.use(notFound)
+
+// app.use((err:any,req:Request,res:Response,next:NextFunction)=>{
+//  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//         success: false,
+//         statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//         message: err.message,
+//         error: err.stack,
+//       });
+// })
+
+
+app.use(globalErrorHandler)
+
 
 export default app;
